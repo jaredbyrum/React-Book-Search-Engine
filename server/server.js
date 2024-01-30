397f8fcd9173b5ab09a46d17536206e7bd2b0a84
Jared Byrum <jaredbyrum@gmail.com>
2024-01-30 18:50:54 -0500

    updated server.js to use apollo server and auth.js for graphql

--- server/server.js
+++ server/server.js
@@ -3,19 +3,43 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const app = express();
+//import apollo server and Auth Middleware
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { authMiddleware } = require('./utils/auth');
+// graphql type-defs and resolvers
+const { typedefs, resolvers } = require('./schemas');
+
 const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const server = new ApolloServer({
+  typedefs,
+  resolvers,
 });
+
+const app = express();
+
+const startApolloServer = async () => {
+  await server.start();
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware
+  }));
+
+    if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
+};
+
+// starts our server
+startApolloServer();
